66eff34ea98bb30785e3e63a746f4ce34e00ce7b
Amr Hassan <103396210+amrgelno@users.noreply.github.com>
2026-05-21 03:54:32 +0300

    Update 2026_05_14_182109_editstaff.php
    Update 2026_05_14_182109_editstaff.php

--- database/migrations/2026_05_14_182109_editstaff.php
+++ database/migrations/2026_05_14_182109_editstaff.php
@@ -4,18 +4,20 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
- class editAttendance extends Migration
+ class editstaff extends Migration
 {
     /**
      * Run the migrations.
      */
     public function up()
     {
-        Schema::table('Attendance', function (Blueprint $table) {
+        Schema::table('staff', function (Blueprint $table) {
 
-            $table->renameColumn('user_id', 'staff_id');
-
-
+            //$table->dropColumn('BirthDay');
+            //$table->renameColumn('Number of hours', 'Number_of_hours');
+            //$table->integer('Number_of_hours')->change();
+            //$table->integer('Number_of_days')->nullable()->after('Number_of_hours');
+            $table->integer('mobile')->nullable()->after('Number_of_days');
         });
     }
 
@@ -24,7 +26,7 @@ use Illuminate\Support\Facades\Schema;
      */
     public function down()
     {
-        Schema::table('Attendance', function (Blueprint $table) {
+        Schema::table('staff', function (Blueprint $table) {
             //
         });
     }
