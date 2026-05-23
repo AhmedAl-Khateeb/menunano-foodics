7731cbb1c784bdf0d74a77b4f43c4c37aade7b25
Amr Hassan <103396210+amrgelno@users.noreply.github.com>
2026-05-24 00:44:45 +0300

    update migration 22/5

---
+++ database/New_migrations/2026_05_14_182109_editAttendance.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+ class editAttendance extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::table('Attendance', function (Blueprint $table) {
+
+            $table->renameColumn('user_id', 'staff_id');
+
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down()
+    {
+        Schema::table('Attendance', function (Blueprint $table) {
+            //
+        });
+    }
+};
